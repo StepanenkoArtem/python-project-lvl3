53f7b73f5bb8526544706879fc015e1fc696a0cd
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-02-14 11:14:20 +0200

    Added setup.cfg files with linter settings, updated pyproject.toml (cliclk dependencies was addded)

--- page_loader/scripts/loader.py
+++ page_loader/scripts/loader.py
@@ -2,11 +2,12 @@
 
 
 def main():
-    print("This is the test")
     return 'This is the test'
+
 
 def t():
     return None
+
 
 if __name__ == "__main__":
     main()
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,10 +1,9 @@
-from page_loader.scripts.loader import main
-from page_loader.scripts.loader import t
-
+from page_loader.scripts.loader import main, t
 
 
 def main_test():
     assert main() == 'This is the test'
+
 
 def test_t():
     assert t() == None
