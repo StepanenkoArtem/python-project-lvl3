582d4770fa77e0ba9936ce5e93e01eea49e83806
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-06-18 21:35:46 +0300

    Realize set_new_link_on_doc and referenced tests

--- page_loader/localize.py
+++ page_loader/localize.py
@@ -17,7 +17,7 @@ _DEFAULT_PARSER = 'html_parser'
 _HREF = 'href'
 _SRC = '_src'
 _DEFAULT_SCHEME = 'https://'
-_START_SLASH = '/'
+_LEAD_SLASH = '/'
 
 
 def url_normalize(url, scheme=_DEFAULT_SCHEME):
@@ -38,50 +38,47 @@ def _is_local(resource):
     return True
 
 
-def get_document_host(document_url):
-    """
-    Obtain downloaded document host including protocol.
-
-    Args:
-        document_url (str): Contain url of downloaded document.
-
-    Returns:
-        (str): String contains host ('http(s)://') of downloaded document.
-    """
-    matched_host = match(HOST_PATTERN, url_normalize(document_url))
-    return matched_host.group()
-
-
 def get_resource_path(resource):
     if resource.get(_HREF):
         path = resource.get(_HREF)
     elif resource.get(_SRC):
         path = resource.get(_SRC)
     path = path.rsplit('?')[0]
-    if path.startswith(_START_SLASH):
+    if path.startswith(_LEAD_SLASH):
         return path
-    return ''.join([_START_SLASH, path])
+    return ''.join([_LEAD_SLASH, path])
 
 
-def set_new_resource_link_on_doc(resource, filename):
-    return filename
+def set_new_resource_link_on_doc(resource, new_link):
+    if resource.attrs[_HREF]:
+        resource.attrs[_HREF] = new_link
+    elif resource.attrs(_SRC):
+        resource.attrs[_SRC] = new_link
+
+    return resource
 
 
 def localize(document, output):  # noqa: WPS210
     document_dom = BeautifulSoup(document.content, _DEFAULT_PARSER)
-    document_host = get_document_host(document.url)
+    document_host = match(
+        HOST_PATTERN,
+        url_normalize(document.url),
+    ).group()
+
     resource_list = list(filter(
         _is_local,
         document_dom.find_all(LOCAL_RESOURCES),
     ),
     )
+
     if resource_list:
         resource_dir = filesystem.create_dir(
-            output=_START_SLASH.join(
+            output=_LEAD_SLASH.join(
                 output,
                 hyphenate.make_resource_dir_name(document.url),
             ),
         )
+
     for resource in resource_list:
         resource_path = get_resource_path(resource)
         downloaded_resource = requests.get(
@@ -101,10 +98,14 @@ def localize(document, output):  # noqa: WPS210
                 path=resource_dir,
                 filename=resource_filename,
             )
-            set_new_resource_link_on_doc(resource, resource_filename)
+            set_new_resource_link_on_doc(
+                resource,
+                resource_filename,
+            )
+
     filesystem.save_document(
         document_content=document_dom,
-        path_file=_START_SLASH.join(
+        path_file=_LEAD_SLASH.join(
             output,
             hyphenate.make_document_name(document.url),
         ),
--- tests/test_localize.py
+++ tests/test_localize.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock
+from unittest.mock import MagicMock, Mock
 
 from page_loader import localize
 from tests.fixtures.get_dataset_from_file import get_dataset_from_json
@@ -8,6 +8,7 @@ _DS_GET_DOCUMENT_HOST = 'tests/datasets/localize/get_document_host.json'
 _DS_GET_RESOURCE_PATH = 'tests/datasets/localize/get_resource_path.json'
 _DS_UPDATE_RESOURCE_FILENAME = 'tests/datasets/localize/update_resource_filename.json'  # noqa: E501
 _DS_URL_NORMALIZE = 'tests/datasets/localize/url_norm.json'
+_DS_SET_RESOURCE_LINK = 'tests/datasets/localize/set_new_link.json'
 
 
 def test_url_normalize():  # noqa: D103
@@ -23,11 +24,6 @@ def test_is_local():  # noqa: D103
         assert expected == localize._is_local(resource)  # noqa: WPS437
 
 
-def test_get_document_host():  # noqa: D103
-    for url, host in get_dataset_from_json(_DS_GET_DOCUMENT_HOST):
-        assert host == localize.get_document_host(url)
-
-
 def test_get_resource_path():  # noqa: D103
     for url, path in get_dataset_from_json(_DS_GET_RESOURCE_PATH):
         resource = Mock()
@@ -36,6 +32,14 @@ def test_get_resource_path():  # noqa: D103
         assert path == localize.get_resource_path(resource)
 
 
-def test_update_resource_filename():  # noqa: D103
-    for path, filename in get_dataset_from_json(_DS_UPDATE_RESOURCE_FILENAME):
-        assert filename == localize.set_new_resource_link_on_doc(path)
+def test_set_new_link_on_doc():  # noqa: D103
+    old_resource = MagicMock()
+    old_resource.attrs = {'href': 'old_value'}
+
+    new_resource = MagicMock()
+    new_resource.attrs = {'href': 'new_link'}
+
+    expected = new_resource.attrs
+    actual = localize.set_new_resource_link_on_doc(old_resource, 'new_link').attrs
+
+    assert expected == actual
