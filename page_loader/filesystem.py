c90a97980eebbec6579b3ddac9990b2b6438c5e3
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-06-24 13:59:05 +0300

    Added docstrings to filesystem.oy and hyphenate.py. Add ignore D104 rules for __init_.py to setup.cfg

--- page_loader/filesystem.py
+++ page_loader/filesystem.py
@@ -1,7 +1,11 @@
+# coding=utf-8
+
+"""This module provides file operations."""
+
 import os
 
 # File operation modes
-WRITE = 'w'
+WRITE = 'wb'
 
 
 def create_dir(output):
@@ -9,7 +13,8 @@ def create_dir(output):
     Create destination directory.
 
     Args:
-        output (str): Path for directory
+        output : str
+            Path for directory
 
     Returns:
         (str | Boolean): Returns path if directory created successfully,
@@ -22,6 +27,14 @@ def create_dir(output):
     return False
 
 
-def save_document(document_content, path_file):  # noqa: D103
+def save_document(document_content, path_file):
+    """
+    Save document content to file.
+
+    Args:
+        document_content (class 'bytes'): Content of html document.
+        path_file (str) : Full path to destination file.
+
+    """
     with open(path_file, mode=WRITE) as html_file:
         html_file.write(document_content)
--- page_loader/hyphenate.py
+++ page_loader/hyphenate.py
@@ -1,3 +1,7 @@
+# coding=utf-8
+
+"""This module provides creating 'hyphanated' filenames and paths."""
+
 import re
 
 # Postfixes
@@ -12,15 +16,46 @@ _HYPHEN = '-'
 _LEADING_SLASH = '/'
 
 
-def _hyphenate(sequence):
-    return re.sub(_HYPHENATE_PATTERN, _HYPHEN, sequence)
+def _hyphenate(path):
+    """
+    Replace non-alphanumerical symbols to hyphen.
+
+    Args :
+        path : str
+
+    Returns :
+        str : String with replaced non-alphanumecric symbols
+    """
+    return re.sub(_HYPHENATE_PATTERN, _HYPHEN, path)
 
 
-def _trim_extension(url):
-    return re.sub(_FILE_EXTENSION, '', url.strip(_LEADING_SLASH))
+def _trim_extension(path):
+    """
+    Get resource file basename without file extendion.
+
+    Args :
+        path:
+            path to local resource fils
+
+    Returns:
+        str: string
+            resource file basenamr.
+    """
+    return re.sub(_FILE_EXTENSION, '', path.strip(_LEADING_SLASH))
 
 
 def make_resource_dir_name(document_url):
+    """
+    Create name for directory where resource files will be saved.
+
+    Add '_files' postfix to 'hyphenated' filename
+
+    Args :
+        document_url (str): URL of downloaded document
+
+    Returns :
+        str : String contains directory name with '_files' ending.
+    """
     return '{file_basename}{ext}'.format(
         file_basename=_hyphenate(_trim_extension(document_url)),
         ext=_RES_DIR_POSTFIX,
@@ -28,6 +63,17 @@ def make_resource_dir_name(document_url):
 
 
 def make_resource_filename(resource_path):
+    """
+    Create filename for resource file.
+
+    Args :
+        resource_path : str
+            Contains path to resource files obtained from html document.
+
+    Returns :
+        str : Filename with replaced non-alpanumerical symbols
+        and original filextension.
+    """
     extension = re.search(_FILE_EXTENSION, resource_path).group(0)
     return '{base_path}{ext}'.format(
         base_path=_hyphenate(_trim_extension(resource_path)),
@@ -36,6 +82,16 @@ def make_resource_filename(resource_path):
 
 
 def make_document_name(document_url):
+    """
+    Create hypheneted filename for downloaded document.
+
+    Args :
+        document_url (str) : document url
+
+    Returns :
+        str : Filename with replaced non-alphanumerical symbols
+        and with 'html' - extendion
+    """
     return '{file_basename}{ext}'.format(
         file_basename=_hyphenate(_trim_extension(document_url)),
         ext=_DOC_EXTENSION,
