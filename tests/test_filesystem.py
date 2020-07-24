2a93e3baf958097519aa8ec73ae41c8c4ff1ac1e
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-07-24 21:08:10 +0300

    replaced hardcoded path on test_create_dir_on_readonly_dir

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -14,13 +14,9 @@ def test_create_dir(fs):  # noqa: D103
 
 def test_create_dir_on_readonly_dir(fs):  # noqa: D103
     permissions = 111
-    read_only_dir = '/read_only_dir'
-    test_dir = 'sub_dir'
+    read_only_dir = os.path.join('read_only_dir')
     fs.create_dir(read_only_dir, perm_bits=permissions)
-    full_path = '{read_only}/{some_dir}'.format(
-        read_only=read_only_dir,
-        some_dir=test_dir,
-    )
+    full_path = os.path.join(read_only_dir, 'sub_dir')
     with pytest.raises(PermissionError):
         assert create_dir(full_path)
 
