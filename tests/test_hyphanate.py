9a2fa0c71a75cf0cfc549e5d9cbd529e4d6713b9
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-09-26 20:58:20 +0300

    add parametrizing to test

--- tests/test_hyphanate.py
+++ tests/test_hyphanate.py
@@ -1,32 +1,65 @@
 # coding=utf-8
 
+"""Check replacing non-alphanumeric symbols to dash in paths."""
+
+import json
+
+import pytest
 from page_loader import hyphenate
-from tests.fixtures.get_dataset_from_file import get_dataset_from_json
-
-_DS_MAKE_RES_DIR_NAME = 'tests/datasets/hyphenate/test_make_resource_dir_name.json'  # noqa: E501
-_DS_MAKE_RES_FILENAME = 'tests/datasets/hyphenate/test_make_resource_filename.json'  # noqa: E501
-_DS_MAKE_DOCUMENT_NAME = 'tests/datasets/hyphenate/test_make_document_name.json'  # noqa: E501
 
 
-def test_make_resource_dir_name():  # noqa: D103
-    test_data = get_dataset_from_json(_DS_MAKE_RES_DIR_NAME)
-    for resource_path, expected_dir_name in test_data:
-        assert expected_dir_name == hyphenate.make_resource_dir_name(
-            resource_path,
-        )
+@pytest.mark.parametrize(
+    'resource_path, expected_dir_name',
+    json.load(
+        open('tests/datasets/hyphenate/test_make_resource_dir_name.json'),
+    ).items(),
+)
+def test_make_resource_dir_name(resource_path, expected_dir_name):
+    """
+    Check replacing non-alphanumeric symbols in resource directory name.
+
+    Args:
+        resource_path (str) : origin path.
+        expected_dir_name (str) : expected modified directory name.
+    """
+    assert expected_dir_name == hyphenate.make_resource_dir_name(
+        resource_path,
+    )
 
 
-def test_make_resource_filename():  # noqa: D103
-    test_data = get_dataset_from_json(_DS_MAKE_RES_FILENAME)
-    for resource_path, expected_filename in test_data:
-        assert expected_filename == hyphenate.make_resource_filename(
-            resource_path,
-        )
+@pytest.mark.parametrize(
+    'resource_path, expected_filename',
+    json.load(
+        open('tests/datasets/hyphenate/test_make_resource_filename.json'),
+    ).items(),
+)
+def test_make_resource_filename(resource_path, expected_filename):
+    """
+    Check replacing non-alphanumeric symbols in filename.
+
+    Args:
+        resource_path (str) : origin filename path
+        expected_filename (str) : modified ("dashed") filename
+    """
+    assert expected_filename == hyphenate.make_resource_filename(
+        resource_path,
+    )
 
 
-def test_make_document_name():  # noqa: D103
-    test_data = get_dataset_from_json(_DS_MAKE_DOCUMENT_NAME)
-    for document_path, expected_filename in test_data:
-        assert expected_filename == hyphenate.make_document_name(
-            document_path,
-        )
+@pytest.mark.parametrize(
+    'document_path, expected_filename',
+    json.load(
+        open('tests/datasets/hyphenate/test_make_document_name.json'),
+    ).items(),
+)
+def test_make_document_name(document_path, expected_filename):
+    """
+    Check replacing non-aplhanumeric symbols in document path name.
+
+    Args:
+        document_path (str) : origin document path
+        expected_filename (str) : expected modified filepath
+    """
+    assert expected_filename == hyphenate.make_document_name(
+        document_path,
+    )
