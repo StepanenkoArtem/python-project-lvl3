9d0b12e943c352e5eb8758205c90c5114e5b85f2
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-10-28 19:39:14 +0200

    create setup() function for logging configuring

--- loader/logger.py
+++ loader/logger.py
@@ -3,32 +3,35 @@
 """Confiuring logging."""
 
 import logging
-import logging.config
+import logging.config  # noqa: WPS301
 
 
-def setup(
-    level=logging.WARNING,
-    logfile='loader.log',
-):
-    """Setup logging settings."""
+def setup(level, logfile):
+    """Set up logging settings.
+
+    Args:
+        level : (int) logging level
+        logfile : (str) logfile name
+    """
     # Configure console handler
     console_formatter = logging.Formatter(
         fmt='{levelname}:{message}',
         style='{',
     )
-    console_handler = logging.StreamHandler
+    console_handler = logging.StreamHandler()
     console_handler.setLevel(level)
     console_handler.setFormatter(console_formatter)
 
     #  Configure file handler
     file_handler = logging.FileHandler(logfile)
+    file_handler.setLevel(logging.DEBUG)
     file_formatter = logging.Formatter(
-        fmt='{asctime}:{levelname}:{name}:{message}',
+        fmt='{asctime}:{levelname}:{message}',
         style='{',
     )
     file_handler.setFormatter(file_formatter)
-    file_handler.setLevel(logging.DEBUG)
 
     logging.basicConfig(
         handlers=(console_handler, file_handler),
+        level=logging.DEBUG,
     )
