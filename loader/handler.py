6c49d9dc003128dec0adb63aa04732d5154631bb
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-10-28 19:37:30 +0200

    add parsing functionality

--- loader/handler.py
+++ loader/handler.py
@@ -1,43 +1,15 @@
 # coding=utf-8
 
-"""
-This library provides main functionality of the app.
+"""Parse and handle downloaded HTML document."""
 
-Parce and save downloaded document.
-Obtain, download and save local resources from downloaded document.
-"""
-
-import os
-from urllib.parse import urljoin, urlparse
-
-from bs4 import BeautifulSoup
-from loader import download, filesystem, hyphenate, settings
-from progress.bar import FillingSquaresBar
+from urllib.parse import urlparse
 
 BAR_CAPTION = 'Loading resources'
-DEFAULT_PARSER = 'html.parser'
+RESOURCE_TYPES = ('img', 'script', 'link')
+RESOURCE_REFS = ('src', 'href')
 
 
-def normalize_url(url, scheme='https://'):
-    """
-    Add HTTPS scheme to URL if does not it contains.
-
-    Args:
-        url : str
-            String contains URL
-        scheme : str
-            Scheme which addEXIT_FS_ERRL
-
-    Returns:
-        url : str
-            URL with leading HTTPS scheme
-    """
-    if not url.startswith('http'):
-        return scheme + url
-    return url
-
-
-def is_resource(resource):
+def _is_resource(resource):
     """
     Figure out is the resource local (don't contain host in url).
 
@@ -51,7 +23,7 @@ def is_resource(resource):
             else return False
     """
     for attr in resource.attrs:
-        if attr in settings.RESOURCE_REFS:
+        if attr in {'src', 'href'}:
             url_parts = urlparse(resource.get(attr))
             scheme_n_netloc = url_parts.netloc or url_parts.scheme
             if len(url_parts.path) > 1 and not scheme_n_netloc:
@@ -59,7 +31,7 @@ def is_resource(resource):
     return False
 
 
-def get_path_from_url(resource):
+def get_path(resource):
     """
     Obtain reference path to the local resource file.
 
@@ -72,15 +44,12 @@ def get_path_from_url(resource):
             String contains full path to resource file
             (excluding protocol and host)
     """
-    for attr in settings.RESOURCE_REFS:
-        if resource.has_attr(attr):
-            path = urlparse(resource.get(attr)).path
-    if path.startswith('/'):
-        return path
-    return ''.join(['/', path])
+    for ref in RESOURCE_REFS:
+        if resource.has_attr(ref):
+            return resource.attrs[ref]
 
 
-def set_new_resource_link(resource, new_link):
+def update_resource(resource, new_link):
     """
     Update resource object reference to downloaded file.
 
@@ -94,75 +63,20 @@ def set_new_resource_link(resource, new_link):
         resource : (class 'bs4.element.Tag')
             Updates resource object
     """
-    for attr in settings.RESOURCE_REFS:
+    for attr in RESOURCE_REFS:
         if resource.has_attr(attr):
             resource.attrs[attr] = new_link
     return resource
 
 
-def localize(document, output):  # noqa: WPS210
-    """
-    Parce and save local resources from document.
-
-    Update downloaded document.
+def get_resources(dom):
+    """Update local resources on DOM.
 
     Args:
-        document : (class 'requests.models.Response)
-            Downloaded document object
-        output : str
-            Destination directory for downloaded files
+        dom : (bytes) html content
 
-    Returns: None
-
+    Returns:
+        : (list) list of resources
     """
-    # Get document DOM
-    document_dom = BeautifulSoup(document, DEFAULT_PARSER)
     # Obtain list of local resources
-    local_resources = list(
-        filter(
-            is_resource,
-            document_dom.find_all(settings.LOCAL_RESOURCES),
-        ),
-    )
-
-    # Create directory for resource files if local resources exist
-    if local_resources:
-        resource_dir = os.path.join(
-            output,
-            hyphenate.make_resource_dir_name(document.url),
-        )
-        filesystem.create_dir(resource_dir)
-
-    # Get resources content and save it to local filesystem
-    with FillingSquaresBar(
-        BAR_CAPTION,
-        max=len(local_resources) if local_resources else 1,
-    ) as counter:
-        for resource in local_resources:
-            resource_urlpath = get_path_from_url(resource)
-            resource_filepath = os.path.join(
-                resource_dir,
-                hyphenate.make_resource_filename(resource_urlpath),
-            )
-            try:
-                filesystem.save_document(
-                    document_content=download.download(
-                        url=urljoin(document.url, resource_urlpath),
-                    ).content,
-                    filepath=resource_filepath,
-                )
-            except ConnectionError as e:
-            set_new_resource_link(
-                resource=resource,
-                new_link=resource_filepath,
-            )
-            counter.next()  # noqa: B305
-        counter.finish()
-    # Save modified document
-    filesystem.save_document(
-        document_content=document_dom.encode(formatter=None),
-        filepath=os.path.join(
-            output,
-            hyphenate.make_document_name(document.url),
-        ),
-    )
+    return list(filter(_is_resource, dom.find_all(RESOURCE_TYPES)))
