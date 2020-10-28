f487fbf8c715341fff845c0761ba3d4bd27c18e0
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-10-28 19:33:45 +0200

    move mainflow from script to cli.py

--- loader/scripts/loader.py
+++ loader/scripts/loader.py
@@ -9,17 +9,12 @@ import sys
 from loader import network, storage
 from loader.cli import main
 
-# EXIT CODES
-EXIT_CONNECTION_ERR = 1
-EXIT_FILESYSTEM_ERR = 2
-EXIT_PARAM_ERR = 3
-
 if __name__ == '__main__':
     try:
         main()
     except ValueError:
         logging.error()
-        sys.exit(EXIT_PARAM_ERR)
+        sys.exit(3)
     except network.NetworkError as network_err:
         logging.error(network_err)
         logging.debug(network_err.__cause__)
