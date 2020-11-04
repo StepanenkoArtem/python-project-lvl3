9e08e73991d2d556fecd25c6a2320f0aeac3e15b
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-11-04 11:33:41 +0200

    add test for checking "loglevel" argument validation

--- tests/test_params.py
+++ tests/test_params.py
@@ -10,12 +10,16 @@ PATH_TEST_CASES = (
     '/usr/my.log',
 )
 LEVEL_VALID_CASES = (
-    '10',
-    10,
-    'Warning',
-    'WARNING',
-    'critical',
-    'deBug',
+    ('10', 10),
+    ('20', 20),
+    ('Warning', 'WARNING'),
+    ('WARNING', 'WARNING'),
+    ('critical', 'CRITICAL'),
+    ('deBug', 'DEBUG'),
+    ('info', 'INFO'),
+    ('Not_set', 'NOT_SET'),
+    ('error', 'ERROR'),
+    ('Error', 'ERROR'),
 )
 
 
@@ -32,3 +36,19 @@ def test_check_path(path):
     """
     with pytest.raises(cli.ParamError):
         cli.check_path('', '', path)
+
+
+@pytest.mark.parametrize(
+    'level_string, level_int',
+    LEVEL_VALID_CASES,
+)
+def test_check_level(level_string, level_int):
+    """Validate logging level.
+
+    Args:
+        level_string : str
+            user-input level
+        level_int : int
+            valid logging level in integer
+    """
+    assert cli.check_level('', 'level', level_string) == level_int
