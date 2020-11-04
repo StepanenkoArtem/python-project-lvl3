07a3c79a0263aceb8d226e27428b1f84ef35303b
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-11-04 11:37:59 +0200

    add loglevel validation callback function

--- loader/cli.py
+++ loader/cli.py
@@ -5,7 +5,7 @@ import logging
 import os
 import sys
 import traceback
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urlunsplit
 
 import click
 from bs4 import BeautifulSoup
@@ -38,6 +38,24 @@ class ParamError(Exception):
         return self.message
 
 
+def check_level(ctx, param, value):
+    """Validate logging level argument.
+
+    Args:
+        ctx: click context
+        param : parameter object
+        value : parameter 'loglevel' value
+
+    Returns:
+        : int
+            valid logging level
+    """
+    try:
+        return int(value)
+    except ValueError:
+        return value.upper()
+
+
 def check_path(ctx, param, value):
     """Check is 'output' argument valid.
 
@@ -55,11 +73,11 @@ def check_path(ctx, param, value):
     abs_path = os.path.abspath(os.path.expanduser(value))
     if not os.access(os.path.dirname(abs_path), os.W_OK):
         raise ParamError(
-            '{value} for {param} is not permitted'.format(
+            '{value} for "--{param}" is not permitted'.format(
                 value=value, param=param,
             ),
         ) from PermissionError
-    return value
+    return abs_path
 
 
 @click.command()
@@ -71,8 +89,10 @@ def check_path(ctx, param, value):
 )
 @click.option(
     '--loglevel',
+    type=str,
     help='Set logging level (INFO, DEBUG, WARNING, ERROR or CRITICAL)',
     default=logging.WARNING,
+    callback=check_level,
 )
 @click.option(
     '--logfile',
@@ -102,47 +122,49 @@ def run(url, output, loglevel, logfile):
     # Download page and get DOM
     dom = BeautifulSoup(network.download(url), DEFAULT_PARSER)
 
+    # Split URL to fragments
+    scheme, net_loc, *_ = list(urlparse(url))
+
     # Get resource objects from DOM
     resources = handler.get_resources(dom)
 
-    # Split URL to fragments
-    url_parts = list(urlparse(url))
-
     if resources:
         # Build resource dirname
-        resource_dirname = path.for_resource_dir(url)
+        local_dirname = path.for_resource_dir(url)
         # Create dir for resource inside 'output'
-        storage.mkdir(os.path.join(output, resource_dirname))
+        storage.mkdir(os.path.join(output, local_dirname))
 
-        resource_urls = []
+        web_resource_paths = []
         for resource in resources:
             # Get resource path from resource object
-            resource_path = handler.get_path(resource)
-            # Build resource filename
-            resource_filename = path.for_resource(resource_path)
-
+            web_resource_path = handler.get_path(resource)
+            # Build resource local path
+            local_resource_path = os.path.join(
+                local_dirname,
+                path.for_resource(web_resource_path),
+            )
             # Set local path in resource object
             handler.update_resource(
                 resource=resource,
-                new_link=os.path.join(resource_dirname, resource_filename),
+                new_link=local_resource_path,
             )
-            # Build URL for resource
-            url_parts[2] = resource_path
-            resource_urls.append(urlunparse(url_parts))
-
+            web_resource_paths.append(web_resource_path)
         # Save modified DOM
         storage.save(
             f_content=dom.encode(),
             output=output,
             filename=path.for_page(url),
         )
-        # Download resources and save it
-        for resource_url in tqdm(resource_urls, desc=BAR_DESC):
+        # Download resources
+        for resource_path in tqdm(web_resource_paths, desc=BAR_DESC):
+            resource_url = urlunsplit(
+                [scheme, net_loc, resource_path, None, None],
+            )
             try:
                 storage.save(
                     f_content=network.download(resource_url),
-                    output=os.path.join(output, resource_dirname),
-                    filename=resource_filename,
+                    output=os.path.join(output, local_dirname),
+                    filename=path.for_resource(resource_path),
                 )
             except network.NetworkError:
                 etype, evalue, tb = sys.exc_info()
